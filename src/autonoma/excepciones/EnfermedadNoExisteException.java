19cd15340fb467d9f885416049400814898468d8
Prometeo <jdavidwork1441@gmail.com>
2024-10-18 02:02:13 -0500

    EnfermedadNoExisteException

--- src/autonoma/excepciones/EnfermedadNoExisteException.java
+++ src/autonoma/excepciones/EnfermedadNoExisteException.java
@@ -1,5 +1,12 @@
 package autonoma.excepciones;
 
+/**
+ *
+ * @author Juan David Arcila
+ * @fecha 10/18/2024
+ * @version 1.0.0
+ */
+
 public class EnfermedadNoExisteException extends Exception {
     public EnfermedadNoExisteException(String mensaje) {
         super(mensaje);
